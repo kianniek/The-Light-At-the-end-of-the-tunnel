be5acd4a3acd7740426850c3cd13925964f28c03
Casper van Hien <casper.van.hien@hva.nl>
2022-12-15 16:38:52 +0100

    Rotating Pipe, rotating pipes and rotating platforms

---
+++ The Light (at the end of the tunnel)/Assets/Segment 2/Scripts/LevelTrigger.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelTrigger : MonoBehaviour
+{
+    public bool levelActive = true;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        levelActive = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
