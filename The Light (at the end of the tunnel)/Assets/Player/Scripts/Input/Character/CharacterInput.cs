0dafa102d0d0c63b219b00dfc382e67d95845dd1
Kian <56929026+kianniek@users.noreply.github.com>
2022-12-15 14:24:12 +0100

    Added playerFiles

---
+++ The Light (at the end of the tunnel)/Assets/Player/Scripts/Input/Character/CharacterInput.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+//This abstract character input class serves as a base for all other character input classes;
+//The 'Controller' component will access this script at runtime to get input for the character's movement (and jumping);
+//By extending this class, it is possible to implement custom character input;
+public abstract class CharacterInput : MonoBehaviour
+{
+    public abstract float GetHorizontalMovementInput();
+    public abstract float GetVerticalMovementInput();
+
+    public abstract bool IsJumpKeyPressed();
+    public abstract bool IsSprintKeyPressed();
+}
