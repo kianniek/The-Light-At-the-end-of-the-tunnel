8b764183cd17260d0a749a13048071c0a9fd96b2
Casper van Hien <casper.van.hien@hva.nl>
2022-12-22 15:53:49 +0100

    Reset allows for timer to stay in time. Lowered difficultyspike

--- The Light (at the end of the tunnel)/Assets/Segment 2/Scripts/WaterBehaviour.cs
+++ The Light (at the end of the tunnel)/Assets/Segment 2/Scripts/WaterBehaviour.cs
@@ -6,11 +6,16 @@ using UnityEngine.SceneManagement;
 
 public class WaterBehaviour : MonoBehaviour
 {
-
     private float timer;
     private bool isTriggered;
+    private Vector3 spawnPosition;
+    private Quaternion spawnRotation;
 
+    [SerializeField] Transform player;
     [SerializeField] float deathScreenTime;
+    [SerializeField] PipeRotation[] pipe;
+    [SerializeField] LevelTrigger levelTrigger;
+    [SerializeField] ReverseTrigger reverseTrigger;
 
     // Start is called before the first frame update
     private void OnTriggerEnter(Collider other)
@@ -24,6 +29,8 @@ public class WaterBehaviour : MonoBehaviour
     void Start()
     {
         isTriggered = false;
+        spawnPosition = player.transform.position;
+        spawnRotation = player.transform.rotation;
     }
 
     // Update is called once per frame
@@ -32,7 +39,17 @@ public class WaterBehaviour : MonoBehaviour
         if (isTriggered)
             timer += Time.deltaTime;
 
-        if(timer >= deathScreenTime && isTriggered)
-            SceneManager.LoadScene("Casper");
+        if (timer >= deathScreenTime && isTriggered)
+        {
+            player.transform.position = spawnPosition;
+            player.transform.rotation = spawnRotation;
+            isTriggered = false;
+            timer = 0;
+            for(int i = 0; i < pipe.Length; i++)
+                pipe[i].Reset();
+            levelTrigger.Reset();
+            reverseTrigger.Reset();
+        }
+        //SceneManager.LoadScene("Casper");
     }
 }
