8d207bc1964641953bf45740de6eb765871c775e
YeBoiVoid <jvesch@ziggo.nl>
2023-02-01 13:23:24 +0100

    Added comments to my script

--- The Light (at the end of the tunnel)/Assets/PushFolder/Scripts/SoundManager.cs
+++ The Light (at the end of the tunnel)/Assets/PushFolder/Scripts/SoundManager.cs
@@ -10,12 +10,10 @@ using UnityEngine.UI;
 
 public class SoundManager : MonoBehaviour
 {
-    // definition of a sound for easy management
     [Serializable]
     public struct Sound
     {
         public AudioClip clip;
-        //[Range(0, 1)] public float volume;
         [Range(0, 100)] public float stopAfterSeconds;
         public bool fadeOut;
         public bool Repeat;
@@ -44,16 +42,18 @@ public class SoundManager : MonoBehaviour
     }
     private void Start()
     {
+        //set the adiosource loop bool to the same value we give to the scruct.
         AudioSource.loop = sounds[0].loop;
         //AudioSource.rolloffMode = AudioRolloffMode.Linear;
         if (!savedVolume)
         {
+            //safe the volume for later use
             safeVolume = AudioSource.volume;
             savedVolume = true;
         }
     }
 
-    [SerializeField]private bool isPlayed = false;
+    [SerializeField] private bool isPlayed = false;
     private void OnTriggerEnter(Collider other)
     {
 
@@ -95,39 +95,48 @@ public class SoundManager : MonoBehaviour
         else
             AudioSource.Pause();
     }
-
+    //not in use
     internal bool GetMusicIsPlaying()
     {
+        //see if there is music playing
         return AudioSource.isPlaying;
     }
 
+    //not in use
     internal float GetMusicPlaytime()
     {
+        //see how long the music has been playing for
         return AudioSource.time;
     }
 
     internal void StopMusic()
     {
+        //stops the music and set isPlayed to false
         AudioSource.Stop();
         isPlayed = false;
     }
 
     internal void SetMusicClip(AudioClip clip)
     {
+        //set the audioclip to a specific clip
         AudioSource.clip = clip;
     }
     public void SetAndPlayMusic(int soundIndex)
-    { 
+    {
+        //increases the repeatCounter and set the music clip to the current sound
         repeatCounter++;
         SetMusicClip(sounds[soundIndex].clip);
 
-        if(sounds[soundIndex].stopAfterSeconds > 0)
+        // check if we want the music to stop after a couple seconds
+        if (sounds[soundIndex].stopAfterSeconds > 0)
         {
+            //if so start the coroutine
             PlayMusic(true);
             StartCoroutine(StopPlayingAfterSeconds(sounds[soundIndex].stopAfterSeconds, sounds[soundIndex].fadeOut));
         }
         else
         {
+            //if not just continue playing until the sound stops
             PlayMusic(true);
             isPlayed = false;
         }
@@ -136,40 +145,41 @@ public class SoundManager : MonoBehaviour
     public bool boolcheck;
     internal IEnumerator StopPlayingAfterSeconds(float playTime, bool fadeOut)
     {
+        //boolcheck to only do these functions once
         if (!boolcheck)
         {
             StartCoroutine(MuteForSeconds(0.1f));
-            StartCoroutine(HalfVolume(playTime / 2));
             boolcheck = true;
         }
-
-        startCountDown = true;
+        //ready the countdown
         yield return new WaitForSeconds(playTime);
+        //after the given playtime for the music check if we want a fadeout 
+        //if we want a countdown continue
         if (fadeOut)
         {
-            if (startCountDown)
+            //if the volume is higher than 0, gradually lower it to simulate a fadeout
+            if (AudioSource.volume > 0)
             {
-
-                if (AudioSource.volume > 0)
-                {
-                    AudioSource.volume -= 0.35f * Time.deltaTime;
-                }
-                if (AudioSource.volume <= 0)
-                {
-                    StopMusic();
-                    StopAllCoroutines();
-                    startCountDown = false;
-                    yield return null;
-                }
+                AudioSource.volume -= 0.35f * Time.deltaTime;
+            }
+            //if the volume is 0, stop playing and stop all coroutines
+            if (AudioSource.volume <= 0)
+            {
+                StopMusic();
+                StopAllCoroutines();
+                startCountDown = false;
+                yield return null;
             }
         }
         else
         {
+            //if we dont want a fadeout just stop the music after the playtime
             StopMusic();
             StopAllCoroutines();
             yield return null;
         }
     }
+    //not in use
     private IEnumerator HalfVolume(float waitBeforeHalfing)
     {
         yield return new WaitForSeconds(waitBeforeHalfing);
@@ -178,13 +188,18 @@ public class SoundManager : MonoBehaviour
     }
     private IEnumerator MuteForSeconds(float wait)
     {
+        //mute the sound for a fraction of a second to prevent the pop
+        //we sometimes hear when a sound is played
         AudioSource.volume = 0;
         yield return new WaitForSeconds(wait);
         AudioSource.volume = safeVolume;
 
     }
     public void Reset()
-    {
+    {   
+        //stop the music, all coroutines
+        //reset the volume and put the repeat counter to 0
+        //set the isPlayed bool to false
         StopMusic();
         StopAllCoroutines();
         AudioSource.volume = safeVolume;
